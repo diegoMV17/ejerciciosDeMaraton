b6cae783af112a43780a198fd4ecb9f744d36104
diegoMV17 <diegomoreno3245@gmail.com>
2025-04-10 15:14:44 -0500

    numeroMasAlto

---
+++ ciudadGoticaNumeroMasalto.py
@@ -0,0 +1,13 @@
+iteration = int(input())
+masalto = {}
+altitud = []
+for i in range(iteration):
+    edificio = input()
+    ed,al = edificio.split()
+    al = int(al)
+    if al not in masalto:
+        masalto[al] = ed
+        altitud.append(al)
+    else:
+        masalto[al] = max(masalto[al], ed)
+print(masalto[max(altitud)])
